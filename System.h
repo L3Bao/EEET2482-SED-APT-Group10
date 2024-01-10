316f4555eec088fe6d08a2dc606a3abc9f696530
Vinh Gia Bao Nguyen <s3986287@rmit.edu.vn>
2024-01-10 11:03:07 +0700

    Change "Motorbike" to "Skill, change "Date" to "DateTime"

--- System.h
+++ System.h
@@ -24,7 +24,97 @@ public:
 
     System();
 
-    
+        void setCurrentMember(Member *member);
+
+    void clearScreen();
+
+    void highlighter();
+
+    void addMember(Member *member);
+
+    void addMotorbike(Motorbike *motorbike);
+
+    void addAdmin(Admin *admin);
+
+    bool memberLogin();
+
+    bool memberSearchSuitableMotorbikeList(Date *startDate, Date *endDate, int cityID);
+
+    bool isSuitableMotorbike(Date *startDate, Date *endDate, int cityID, Motorbike *motorbike);
+
+    bool memberSendRequest(Date *startDate, Date *endDate, int motorbikeID);
+
+    bool memberSeeRequestList();
+
+    bool memberAcceptRequest(int requestID);
+
+    bool memberViewRentList();
+
+    bool turnbackMotorbike(int turnbackRentMotorbikeID);
+
+    bool memberViewUnratedRenterList();
+
+    bool memberRateRenter(int renterID);
+
+    bool memberRateMotorbike(Motorbike* ratedMotorbike);
+
+    void mainMenu();
+
+    void adminMenu();
+
+    bool adminLogin();
+
+    void adminViewMemberList();
+
+    void adminViewMotorbikeList();
+
+    void memberMenu();
+
+    void memberLoginMenu();
+
+    bool memberRegister();
+
+    void memberListMenu();
+
+    bool memberListMotorbike();
+
+    bool memberUnlistMotorbike();
+
+    void memberSearchSuitableMotorbikeMenu();
+
+    void memberSuitableMotorbikeMenu(Date *sD, Date *eD, int cityID);
+
+    void memberViewMotorbikeRequestList(int motorbikeID);
+
+    bool memberViewMotorbikeReviewList(int motorbikeID, Date *sD, Date *eD);
+
+    void memberUnratedRenterRatingMenu(int member);
+
+    void memberViewRentedMotorbike();
+
+    bool memberLeaveReview(Motorbike *turnbackMotorbike);
+
+    bool memberEnterMotorbikeInfo();
+
+    void guestMenu();
+
+    void guestViewMotorbikeList();
+
+    void UnrateMemRatingMenu();
+
+    void memberViewMotorbikeRequestListMenu();
+
+    void suitableMotorbikeMenu(int choice, Date *sD, Date *eD, int cityID);
+
+    void memberViewRenterReviewList(int choice);
+
+    int choiceFunc(int a, int b);
+
+    void systemExit();
+
+    friend class InputStorageManager;
+
+    friend class OutputStorageManager;
 
 };
 
